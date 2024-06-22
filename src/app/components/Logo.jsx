db6d10e1a54484cd2eb1124559bfcbb2dba968b7
pedromax23 <pedromorgade1234@gmail.com>
2024-06-22 17:40:11 -0300

    alert in the logo

--- src/app/components/Logo.jsx
+++ src/app/components/Logo.jsx
@@ -1,9 +1,11 @@
+"use client"
 import Image from "next/image";
 
-
 function Logo() {
+
     return (
         <Image
+            onClick={() => {alert('bostero hijo eterno')}}
             className="dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert z-10"
             src="/image/sanlorenzo.png"
             alt="Next.js Logo"
